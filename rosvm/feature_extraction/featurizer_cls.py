a09e614260c7ad2c997b032434e6be5ddec39492
Eric Bach <eric.bach@aalto.fi>
2020-05-08 12:04:53 +0300

    Updates to the circular featurizer, implemented tests

--- rosvm/feature_extraction/featurizer_cls.py
+++ rosvm/feature_extraction/featurizer_cls.py
@@ -2,7 +2,7 @@
 #
 # The MIT License (MIT)
 #
-# Copyright 2019, 2020 Eric Bach <eric.bach@aalto.fi>
+# Copyright 2020 Eric Bach <eric.bach@aalto.fi>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -25,25 +25,27 @@
 ####
 import numpy as np
 
+from collections import OrderedDict
 from rdkit.Chem.rdMolDescriptors import GetMorganFingerprint, GetMorganFingerprintAsBitVect
 from rdkit.Chem import MolFromSmiles
 from rdkit.DataStructs.cDataStructs import UIntSparseIntVect
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.utils.validation import check_is_fitted
 from scipy.sparse import dok_matrix
 
 
 class CircularFPFeaturizer(BaseEstimator, TransformerMixin):
-    def __init__(self, fp_type="ECFP", only_freq_subs=False, min_subs_freq=0.1, fp_mode="count", n_bits=2048,
-                 use_chirality=False):
+    def __init__(self, fp_type="ECFP", only_freq_subs=False, min_subs_freq=0.1, fp_mode="count", n_bits_folded=2048,
+                 use_chirality=False, output_dense_matrix=False, max_n_bits_for_dense_output=10000):
         """
         Circular Fingerprint featurizer calculates ECFP or FCFP fingerprints from molecular structures.
 
-        :param fp_type: string, specifying whether "ECFP" or "FCFP" fingerprints should be used.
+        :param fp_type: string, specifying whether "ECFP", "ecfp", "FCFP" or "fcfp" fingerprints should be used.
         :param only_freq_subs: boolean, indicating whether a set of frequently appearing sub-structures should be
             learned on the molecule set passed to 'fit'. If True, the transform function only returns fingerprints that
             are frequent on the training set. Otherwise, molecule set passed to 'fit' are ignored. The minimum frequency
             of a sub-structure to be used can be specified using 'min_subs_freq'.
-        :param min_subs_freq: scalar, from range (0, 1) specifying the required frequency of a sub-structure appearing
+        :param min_subs_freq: scalar, from range [0, 1] specifying the required frequency of a sub-structure appearing
             in the training set (passed to 'fit') to be included in the final fingerprint. E.g. a value of 0.1 means,
             that a sub-structure needs to appear in at least 10% of the training molecules.
         :param fp_mode: string, specifying whether "count", "binary" or "binary_folded" fingerprints should be
@@ -54,18 +56,27 @@ class CircularFPFeaturizer(BaseEstimator, TransformerMixin):
                 "binary_folded": Return a fixed length binary vector (see 'n_bits') where the sub-structure hashes
                 are folded to achieve a fixed length output. This option does not use the concept of frequent
                 sub-structures, i.e. training molecules are ignored.
-        :param n_bits: scalar, number of bits used for the fixed length binary fingerprint vector, when
+        :param n_bits_folded: scalar, number of bits used for the fixed length binary fingerprint vector, when
             'fp_type="binary_folded"'.
         :param use_chirality: boolean, indicating whether chirality information should be added to the generated
             fingerprint
+        :param output_dense_matrix: boolean, indicating whether the returned fingerprint matrix should be dense.
+            If False, the returned matrix will be of type "scipy.sparse.csr_matrix". This parameter should be used with
+            caution. When hashed fingerprints are used, e.g. counting ECFP, than the output matrix dimension is
+            (n_molecules, 2 ** 32), so huge! However, when frequent sub-sets are used, than the output dimension could
+            be much smaller. There is a save-guard installed, that always returns a sparse matrix if the number of
+            fingerprint bits would be larger 10000 (see "max_n_bits_for_dense_output").
+        :param max_n_bits_for_dense_output: scalar, maximum number of bits allowed to convert output to a dense matrix.
+            This parameter is used to override "return_dense_matrix" at run-time.
         """
+        fp_type = fp_type.upper()
         if fp_type not in ["ECFP", "FCFP"]:
             raise ValueError("Invalid fingerprint type: '%s'. Choices are 'ECFP' and 'FCFP'.")
         self.fp_type = fp_type
         self._use_features = (self.fp_type == "FCFP")
 
-        if min_subs_freq <= 0 or min_subs_freq >= 1:
-            raise ValueError("Sub-structure frequency invalid: '%f'. Must be from range (0, 1).")
+        if min_subs_freq < 0 or min_subs_freq > 1:
+            raise ValueError("Sub-structure frequency invalid: '%f'. Must be from range [0, 1].")
         self.min_subs_freq = min_subs_freq
 
         if fp_mode not in ["count", "binary", "binary_folded"]:
@@ -74,20 +85,38 @@ class CircularFPFeaturizer(BaseEstimator, TransformerMixin):
         self._use_counts = (self.fp_mode == "count")
         self._use_binary_generator = (self.fp_mode == "binary_folded")
 
-        self.only_freq_subs = only_freq_subs
-        self.n_bits = n_bits
+        self.only_freq_subs = only_freq_subs if not self._use_binary_generator else False
+        self.n_bits_folded = n_bits_folded
         self.use_chirality = use_chirality
+        self.return_dense_matrix = output_dense_matrix
+        self.max_n_bits_for_dense_output = max_n_bits_for_dense_output
 
         self._radius = 2
         self._max_hash_value = 2 ** 32  # hash values are stored as unsigned int (32 bit), uint32
 
-        self._freq_hash_set = None
+        # Parameters to fit
+        if self._use_binary_generator:
+            self.n_bits_ = self.n_bits_folded
+        else:
+            if not only_freq_subs:
+                self.n_bits_ = self._max_hash_value
+
+    def get_length(self):
+        return self.__len__()
+
+    def __len__(self):
+        check_is_fitted(self, ["n_bits_"],
+                        "When using frequent substructure sets, the 'fit' function must be called on a set of "
+                        "molecular training structures.")
+
+        return self.n_bits_
 
     def _get_fingerprint(self, mol):
         """
         Calculate the fingerprint for a molecule according to the specifications.
 
-        :param mol: rdkit.Chem.rdchem.Mol object, molecule representation
+        :param mol: string or rdkit.Chem.rdchem.Mol object, a string is interpreted as SMILES representing
+            the molecule and converted into an rdkit mol object.
 
         :return:
             rdkit.DataStructs.cDataStructs.UIntSparseIntVect, if fp_mode is "count" or "binary"
@@ -98,11 +127,14 @@ class CircularFPFeaturizer(BaseEstimator, TransformerMixin):
         """
         if isinstance(mol, str):
             # Convert SMILES to mol objects if strings are provided.
-            mol = MolFromSmiles(mol)
+            smi = mol
+            mol = MolFromSmiles(smi)
+            if not mol:
+                raise RuntimeError("SMILES could not be parsed: '%s'." % smi)
 
         if self._use_binary_generator:
             fp = GetMorganFingerprintAsBitVect(mol, radius=self._radius, useChirality=self.use_chirality,
-                                               useFeatures=self._use_features)
+                                               useFeatures=self._use_features, nBits=self.n_bits_folded)
         else:
             fp = GetMorganFingerprint(mol, radius=self._radius, useChirality=self.use_chirality,
                                       useFeatures=self._use_features, useCounts=self._use_counts)
@@ -124,7 +156,7 @@ class CircularFPFeaturizer(BaseEstimator, TransformerMixin):
         :param mols: list of strings or rdkit.Chem.rdchem.Mol objects, strings are interpreted as SMILES representing
             the molecules and converted into rdkit mol objects.
         """
-        if not self.only_freq_subs or self.fp_mode == "binary_folded":
+        if not self.only_freq_subs:
             # No fitting needed
             return self
 
@@ -132,50 +164,66 @@ class CircularFPFeaturizer(BaseEstimator, TransformerMixin):
         fps = self._get_fingerprints(mols)
         assert isinstance(fps[0], UIntSparseIntVect)
 
-        # Count all hashes
-        hash_cnts = dict()
-        for fp in fps:  # O(n_mols)
-            for key in fp.GetNonzeroElements().keys():  # O(max_number_of_hashes) but in practice < 100 are active
-                if key in hash_cnts:
-                    hash_cnts[key] += 1
-                else:
-                    hash_cnts[key] = 1
-
-        # Keep only frequent hashes
-        n_mols = len(mols)
-        self._freq_hash_set = dict()
-        i = 0
-        for hash, cnt in hash_cnts:
-            if cnt / n_mols >= self.min_subs_freq:
-                self._freq_hash_set[hash] = i
-                i += 1
+        # Count all hashes and filter out infrequent substructures
+        self.freq_hash_set_, self.hash_cnts_filtered_ = self._count_and_filter_hashes(
+            [fp.GetNonzeroElements().keys() for fp in fps], self.min_subs_freq)
+        self.n_bits_ = len(self.freq_hash_set_)
 
         return self
 
     def transform(self, mols):
+        if self.only_freq_subs:
+            check_is_fitted(self, ["n_bits_", "freq_hash_set_", "hash_cnts_filtered_"],
+                            "When using frequent substructure sets, the 'fit' function must be called on a set of "
+                            "molecular training structures.")
+
+        # Calculate fingerprints for the molecules
         fps = self._get_fingerprints(mols)
 
         # Construct sparse matrix from fingerprints
-        n_mols = len(mols)
+        dtype = np.uint16 if self.fp_mode == "count" else np.bool
+        fps_mat = dok_matrix((len(mols), self.n_bits_), dtype=dtype)
         if self.fp_mode == "binary_folded":
-            n_bits = self.n_bits
-            fps_mat = dok_matrix((n_mols, n_bits), dtype="bool")
             for i, fp in enumerate(fps):
                 fps_mat[i, list(fp.GetOnBits())] = True
         else:
-            n_bits = len(self._freq_hash_set) if self.only_freq_subs else self._max_hash_value
-            fps_mat = dok_matrix((n_mols, n_bits), dtype=np.uint16)
             for i, fp in enumerate(fps):
                 for hash, cnt in fp.GetNonzeroElements().items():
                     if self.only_freq_subs:
-                        if hash in self._freq_hash_set:
-                            fps_mat[i, self._freq_hash_set[hash]] = cnt
+                        if hash in self.freq_hash_set_:
+                            fps_mat[i, self.freq_hash_set_[hash]] = cnt
                     else:
                         fps_mat[i, hash] = cnt
 
-        fps_mat = fps_mat.tocsr()
+        if self.return_dense_matrix and (self.n_bits_ <= self.max_n_bits_for_dense_output):
+            fps_mat = fps_mat.todense()
+        else:
+            fps_mat = fps_mat.tocsr()
 
         return fps_mat
+
+    @staticmethod
+    def _count_and_filter_hashes(dicts, min_freq) -> [OrderedDict, OrderedDict]:
+        hash_cnts = OrderedDict()
+        for d in dicts:
+            for h in d:
+                if h in hash_cnts:
+                    hash_cnts[h] += 1
+                else:
+                    hash_cnts[h] = 1
+
+        # Filter the hashes according to the required minimum frequency
+        hash_cnts_filtered = OrderedDict()
+        hash_idc = OrderedDict()  # stores (key, value) pairs: key=fp-hash, value=index-in-fp-matrix
+        i = 0
+        for h, cnt in hash_cnts.items():
+            freq = cnt / len(dicts)
+            if freq >= min_freq:
+                hash_cnts_filtered[h] = (cnt, freq)
+                hash_idc[h] = i
+                i += 1
+
+        return hash_idc, hash_cnts_filtered
 
 
 if __name__ == "__main__":
--- rosvm/feature_extraction/tests/unittests_featurizer.py
+++ rosvm/feature_extraction/tests/unittests_featurizer.py
@@ -1,9 +1,288 @@
 import unittest
+import numpy as np
+
+from collections import OrderedDict
+from rdkit.Chem import MolFromSmiles
+from rdkit.Chem.rdMolDescriptors import GetMorganFingerprint, GetMorganFingerprintAsBitVect
+from scipy.sparse import isspmatrix_csr
+from sklearn.exceptions import NotFittedError
+
+from featurizer_cls import CircularFPFeaturizer
 
 
-class CircularFPFeaturizer(unittest.TestCase):
-    def test_ecfp(self):
-        self.assertEqual(True, False)
+class TestCircularFPFeaturizer(unittest.TestCase):
+    def setUp(self) -> None:
+        self.smis = ["CC(=O)C1=CC2=C(OC(C)(C)[C@@H](O)[C@@H]2O)C=C1",
+                     "C1COC2=CC=CC=C2C1",
+                     "O=C(CCc1ccc(O)cc1)c1c(O)cc(O)c(C2OC(CO)C(O)C(O)C2O)c1O",
+                     "O=c1c(OC2OC(CO)C(O)C(O)C2O)c(-c2ccc(OC3OC(CO)C(O)C(O)C3O)c(O)c2)oc2cc(O)cc(O)c12",
+                     "O=C(O)C1OC(Oc2c(-c3ccc(O)c(O)c3)oc3cc(O)cc(O)c3c2=O)C(O)C(O)C1O",
+                     "Oc1cc(O)c2c(c1)OC1(c3ccc(O)c(O)c3)Oc3cc(O)c4c(c3C2C1O)OC(c1ccc(O)c(O)c1)C(O)C4",
+                     "COc1cc(O)c2c(=O)c(O)c(-c3ccc(O)c(O)c3)oc2c1",
+                     "CC1OC(O)C(O)C(O)C1O",
+                     "Cc1cc2nc3c(O)nc(=O)nc-3n(CC(O)C(O)C(O)CO)c2cc1C",
+                     "O=C(C=Cc1ccc(O)c(O)c1)OC(Cc1ccc(O)c(O)c1)C(=O)O",
+                     "COc1cc(O)c2c(c1)OC(c1ccc(O)cc1)CC2=O",
+                     "C=CC(C)(O)CCC1C(C)(O)CCC2C(C)(C)CCCC21C",
+                     "COc1cc2ccc(=O)oc2cc1O",
+                     "NCCc1c[nH]c2ccc(O)cc12"]
+        self.mols = [MolFromSmiles(smi) for smi in self.smis]
+        self.n_mols = len(self.mols)
+
+    def test__error_when_parsing_smiles(self) -> None:
+        with self.assertRaises(RuntimeError):
+            CircularFPFeaturizer().transform([
+                "O=C(O)C1OC(Oc2c(-c3ccc(O)c(O)c3)oc3cc(O)cc(O)c3c2=O)C(O)C(O)C1O",
+                "Oc1cc(O)c2c(c1)OC1(c3ccc(O)c(O)c3)Oc3cc(O)c4c(c3C2C1O)OC(c1ccc(O)c(O)c1)C(O)C4",
+                "COc1cc(O)c2c(=O)c(O)c(-c3ccc(O)c(O)c3)oc2c1",
+                "CaC1asfOC(O)C(O)C(O)C1O"])
+
+    def test__count_and_filter_hashes(self) -> None:
+        d = [
+            {"A": 0, "B": 0, "C": 0, "D": 0},
+            {"A": 0 ,        "C": 0, "D": 0},
+            {        "B": 0, "C": 0, "D": 0},
+            {        "B": 0, "C": 0, "D": 0},
+            {        "B": 0, "C": 0, "D": 0},
+            {        "B": 0, "C": 0, "D": 0},
+            {"A": 0, "B": 0, "C": 0, "D": 0},
+            {                        "D": 0}
+        ]  # A = 3 / 8, B = 6 / 8 and C = 7 / 8
+
+        # Must appear in at least 101% --> should result in an empty output
+        self.assertEqual(len(CircularFPFeaturizer._count_and_filter_hashes(d, 1.01)[0]), 0)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 1.01)[0], OrderedDict())
+
+        # Must appear in at least 0%
+        self.assertEqual(len(CircularFPFeaturizer._count_and_filter_hashes(d, 0)[0]), 4)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 0)[0].keys(), {"A", "B", "C", "D"})
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 0)[0]["A"], 0)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 0)[0]["B"], 1)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 0)[0]["C"], 2)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 0)[0]["D"], 3)
+
+        self.assertEqual(len(CircularFPFeaturizer._count_and_filter_hashes(d, 2 / 8)[0]), 4)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 2 / 8)[0].keys(), {"A", "B", "C", "D"})
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 2 / 8)[0]["A"], 0)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 2 / 8)[0]["B"], 1)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 2 / 8)[0]["C"], 2)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 2 / 8)[0]["D"], 3)
+
+        self.assertEqual(len(CircularFPFeaturizer._count_and_filter_hashes(d, 3 / 8)[0]), 4)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 3 / 8)[0].keys(), {"A", "B", "C", "D"})
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 3 / 8)[0]["A"], 0)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 3 / 8)[0]["B"], 1)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 3 / 8)[0]["C"], 2)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 3 / 8)[0]["D"], 3)
+
+        self.assertEqual(len(CircularFPFeaturizer._count_and_filter_hashes(d, 4 / 8)[0]), 3)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 4 / 8)[0].keys(), {"B", "C", "D"})
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 4 / 8)[0]["B"], 0)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 4 / 8)[0]["C"], 1)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 4 / 8)[0]["D"], 2)
+
+        self.assertEqual(len(CircularFPFeaturizer._count_and_filter_hashes(d, 6 / 8)[0]), 3)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 6 / 8)[0].keys(), {"B", "C", "D"})
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 6 / 8)[0]["B"], 0)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 6 / 8)[0]["C"], 1)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 6 / 8)[0]["D"], 2)
+
+        self.assertEqual(len(CircularFPFeaturizer._count_and_filter_hashes(d, 7 / 8)[0]), 2)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 7 / 8)[0].keys(), {"C", "D"})
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 7 / 8)[0]["C"], 0)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 7 / 8)[0]["D"], 1)
+
+        self.assertEqual(len(CircularFPFeaturizer._count_and_filter_hashes(d, 8 / 8)[0]), 1)
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 8 / 8)[0].keys(), {"D"})
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes(d, 8 / 8)[0]["D"], 0)
+
+        # Empty input
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes({}, 0)[0], OrderedDict())
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes({}, 0.5)[0], OrderedDict())
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes({}, 1)[0], OrderedDict())
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes({}, 0)[1], OrderedDict())
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes({}, 0.5)[1], OrderedDict())
+        self.assertEqual(CircularFPFeaturizer._count_and_filter_hashes({}, 1)[1], OrderedDict())
+
+    def test__train_with_frequent_substrset(self) -> None:
+        # appears on ALL molecules
+        self.assertEqual(len(CircularFPFeaturizer(only_freq_subs=True, min_subs_freq=1).fit(self.mols)), 0)
+
+        # All data for fit and transform
+        n_old = np.inf
+        for freq in np.arange(0, 1.01, 0.01):
+            fprinter = CircularFPFeaturizer(only_freq_subs=True, min_subs_freq=freq).fit(self.mols)
+
+            # set of frequent pattern should get smaller when we require the patterns to be appear in more molecules
+            n_new = len(fprinter)
+            self.assertTrue(n_new <= n_old)
+            n_old = n_new
+
+            # Check dimension of transformed output
+            fps_mat = fprinter.transform(self.mols)
+            self.assertEqual(len(fprinter), fps_mat.shape[1])
+
+            # Check frequency of substructures in the output
+            freq_hash_set_inv = {v: k for k, v in fprinter.freq_hash_set_.items()}
+            for j in range(len(fprinter)):
+                h = freq_hash_set_inv[j]
+                self.assertTrue(np.sum(fps_mat[:, j].data > 0) / self.n_mols >= fprinter.hash_cnts_filtered_[h][1])
+
+        # Half of the data for fit and the other half for transform
+        n_old = np.inf
+        for freq in np.arange(0, 1.01, 0.01):
+            fprinter = CircularFPFeaturizer(only_freq_subs=True, min_subs_freq=freq).fit(self.mols[:7])
+
+            # set of frequent pattern should get smaller when we require the patterns to be appear in more molecules
+            n_new = len(fprinter)
+            self.assertTrue(n_new <= n_old)
+            n_old = n_new
+
+            # Check dimension of transformed output
+            fps_mat = fprinter.transform(self.mols[7:])
+            self.assertEqual(len(fprinter), fps_mat.shape[1])
+
+    def test__determine_not_fitted_yet(self) -> None:
+        fprinter = CircularFPFeaturizer(only_freq_subs=True)
+
+        with self.assertRaises(NotFittedError):
+             len(fprinter)
+        with self.assertRaises(NotFittedError):
+            fprinter.transform(self.mols)
+
+        self.assertEqual(len(fprinter.fit(self.mols)), 86)
+
+    def test__to_dense_output(self) -> None:
+        # Output to large to be converted to a dense matrix
+        fpr_mat = CircularFPFeaturizer(fp_mode="binary_folded", output_dense_matrix=True, n_bits_folded=2048,
+                                       max_n_bits_for_dense_output=2048).fit_transform(self.mols)
+        self.assertFalse(isspmatrix_csr(fpr_mat))
+        self.assertTrue(isinstance(fpr_mat, np.ndarray))
+
+        # Output to large to be converted to a dense matrix
+        fpr_mat = CircularFPFeaturizer(fp_mode="binary_folded", output_dense_matrix=True, n_bits_folded=2048,
+                                       max_n_bits_for_dense_output=100).fit_transform(self.mols)
+        self.assertTrue(isspmatrix_csr(fpr_mat))
+        self.assertFalse(isinstance(fpr_mat, np.ndarray))
+
+        # Save-guard works for hashed fingerprints
+        fpr_mat = CircularFPFeaturizer(output_dense_matrix=True).fit_transform(self.mols)
+        self.assertTrue(isspmatrix_csr(fpr_mat))
+        self.assertFalse(isinstance(fpr_mat, np.ndarray))
+
+    def test__hashed_counting_fingerprints__ecfp(self) -> None:
+        fprintr = CircularFPFeaturizer()
+
+        fps_mat_smi = fprintr.fit_transform(self.smis)  # using SMILES
+        fps_mat_mol = fprintr.fit_transform(self.mols)  # using Mol objects
+
+        # Output shape
+        self.assertEqual(fps_mat_smi.shape[0], self.n_mols)
+        self.assertEqual(fps_mat_smi.shape[1], fprintr._max_hash_value)
+        self.assertEqual(fps_mat_mol.shape[0], self.n_mols)
+        self.assertEqual(fps_mat_mol.shape[1], fprintr._max_hash_value)
+
+        # Fingerprint matrix structure
+        for i, mol in enumerate(self.mols):
+            fps_ref = GetMorganFingerprint(mol, radius=fprintr._radius, useFeatures=fprintr._use_features,
+                                           useChirality=fprintr.use_chirality, useCounts=fprintr._use_counts)
+            for hash, cnt in fps_ref.GetNonzeroElements().items():
+                self.assertEqual(fps_mat_smi[i, hash], cnt)
+                self.assertEqual(fps_mat_mol[i, hash], cnt)
+
+    def test__hashed_counting_fingerprints__fcfp(self) -> None:
+        fprintr = CircularFPFeaturizer(fp_type="fcfp")
+
+        fps_mat_smi = fprintr.fit_transform(self.smis)  # using SMILES
+        fps_mat_mol = fprintr.fit_transform(self.mols)  # using Mol objects
+
+        # Output shape
+        self.assertEqual(fps_mat_smi.shape[0], self.n_mols)
+        self.assertEqual(fps_mat_smi.shape[1], fprintr._max_hash_value)
+        self.assertEqual(fps_mat_mol.shape[0], self.n_mols)
+        self.assertEqual(fps_mat_mol.shape[1], fprintr._max_hash_value)
+
+        # Fingerprint matrix structure
+        for i, mol in enumerate(self.mols):
+            fps_ref = GetMorganFingerprint(mol, radius=fprintr._radius, useFeatures=fprintr._use_features,
+                                           useChirality=fprintr.use_chirality, useCounts=fprintr._use_counts)
+            for hash, cnt in fps_ref.GetNonzeroElements().items():
+                self.assertEqual(fps_mat_smi[i, hash], cnt)
+                self.assertEqual(fps_mat_mol[i, hash], cnt)
+
+    def test__hashed_binary_fingerprints__ecfp(self) -> None:
+        fprintr = CircularFPFeaturizer(fp_mode="binary")
+
+        fps_mat_smi = fprintr.fit_transform(self.smis)  # using SMILES
+        fps_mat_mol = fprintr.fit_transform(self.mols)  # using Mol objects
+
+        # Output shape
+        self.assertEqual(fps_mat_smi.shape[0], self.n_mols)
+        self.assertEqual(fps_mat_smi.shape[1], fprintr._max_hash_value)
+        self.assertEqual(fps_mat_mol.shape[0], self.n_mols)
+        self.assertEqual(fps_mat_mol.shape[1], fprintr._max_hash_value)
+
+        # Fingerprint matrix structure
+        for i, mol in enumerate(self.mols):
+            fps_ref = GetMorganFingerprint(mol, radius=fprintr._radius, useFeatures=fprintr._use_features,
+                                           useChirality=fprintr.use_chirality, useCounts=fprintr._use_counts)
+            for hash in fps_ref.GetNonzeroElements():
+                self.assertTrue(fps_mat_smi[i, hash])
+                self.assertTrue(fps_mat_mol[i, hash])
+
+            # No other elements are set
+            self.assertEqual(np.sum(fps_mat_smi[i, :].data), len(fps_ref.GetNonzeroElements()))
+            self.assertEqual(np.sum(fps_mat_mol[i, :].data), len(fps_ref.GetNonzeroElements()))
+
+    def test__folded_binary_fingerprints__ecfp(self) -> None:
+        fprintr = CircularFPFeaturizer(fp_mode="binary_folded", n_bits_folded=512)
+
+        fps_mat_smi = fprintr.fit_transform(self.smis)  # using SMILES
+        fps_mat_mol = fprintr.fit_transform(self.mols)  # using Mol objects
+
+        # Output shape
+        self.assertEqual(fps_mat_smi.shape[0], self.n_mols)
+        self.assertEqual(fps_mat_smi.shape[1], fprintr.n_bits_folded)
+        self.assertEqual(fps_mat_mol.shape[0], self.n_mols)
+        self.assertEqual(fps_mat_mol.shape[1], fprintr.n_bits_folded)
+
+        # Fingerprint matrix structure
+        for i, mol in enumerate(self.mols):
+            fps_ref = GetMorganFingerprintAsBitVect(mol, radius=fprintr._radius, useFeatures=fprintr._use_features,
+                                                    useChirality=fprintr.use_chirality, nBits=fprintr.n_bits_folded)
+            on_bits = list(fps_ref.GetOnBits())
+            for j in range(fprintr.n_bits_folded):
+                if j in on_bits:
+                    self.assertTrue(fps_mat_smi[i, j])
+                    self.assertTrue(fps_mat_mol[i, j])
+                else:
+                    self.assertFalse(fps_mat_smi[i, j])
+                    self.assertFalse(fps_mat_mol[i, j])
+
+    def test__rdkit_parameters_are_correct(self) -> None:
+        fprintr = CircularFPFeaturizer(fp_type="ecfp", fp_mode="count")
+        self.assertTrue(fprintr._use_counts)
+        self.assertFalse(fprintr._use_features)
+
+        fprintr = CircularFPFeaturizer(fp_type="fcfp", fp_mode="count")
+        self.assertTrue(fprintr._use_counts)
+        self.assertTrue(fprintr._use_features)
+
+        fprintr = CircularFPFeaturizer(fp_type="ecfp", fp_mode="binary")
+        self.assertFalse(fprintr._use_counts)
+        self.assertFalse(fprintr._use_features)
+
+        fprintr = CircularFPFeaturizer(fp_type="fcfp", fp_mode="binary")
+        self.assertFalse(fprintr._use_counts)
+        self.assertTrue(fprintr._use_features)
+
+        fprintr = CircularFPFeaturizer(fp_type="ecfp", fp_mode="binary_folded")
+        self.assertFalse(fprintr._use_counts)
+        self.assertFalse(fprintr._use_features)
+
+        fprintr = CircularFPFeaturizer(fp_type="fcfp", fp_mode="binary_folded")
+        self.assertFalse(fprintr._use_counts)
+        self.assertTrue(fprintr._use_features)
 
 
 if __name__ == '__main__':
